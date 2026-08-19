4dfb042967d1218439e86b2ebe497fa4acb74ad9
vksvicky <vksvicky@gmail.com>
2026-08-19 21:55:53 +0100

    Add multi-resolution resources and interactive device menu to build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,35 @@
+#!/bin/bash
 # Kill existing simulator instances to prevent connection issues
 pkill -f connectiq || true
 sleep 1
 
+DEVICE="$1"
+
+if [ -z "$DEVICE" ]; then
+    echo "Select a target device to build for:"
+    devices=("fenix7" "epix2" "venu3" "enduro3" "fenix847mm" "Quit")
+    select sel in "${devices[@]}"; do
+        if [ "$sel" = "Quit" ]; then
+            echo "Exiting."
+            exit 0
+        elif [ -n "$sel" ]; then
+            DEVICE="$sel"
+            break
+        else
+            echo "Invalid selection. Please enter a number from the menu."
+        fi
+    done
+fi
+
 # Clean bin directory
 rm -rf "/Users/vivek/Development/GarminWatchFace/bin"
-
-# Create bin directory
 mkdir -p "/Users/vivek/Development/GarminWatchFace/bin"
+
+echo "Building watch face for $DEVICE..."
 
 # Compile the watch face
 "/Users/vivek/Library/Application Support/Garmin/ConnectIQ/Sdks/connectiq-sdk-mac-8.0.0_Beta-2025-01-07-276916717/bin/monkeyc" \
-    -d epix2 \
+    -d "$DEVICE" \
     -f monkey.jungle \
     -o bin/GarminWatchFace.prg \
     -y developer_key.der
@@ -21,4 +40,4 @@ mkdir -p "/Users/vivek/Development/GarminWatchFace/bin"
 sleep 5
 "/Users/vivek/Library/Application Support/Garmin/ConnectIQ/Sdks/connectiq-sdk-mac-8.0.0_Beta-2025-01-07-276916717/bin/monkeydo" \
     "/Users/vivek/Development/GarminWatchFace/bin/GarminWatchFace.prg" \
-    epix2
+    "$DEVICE"
