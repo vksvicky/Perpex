981ba8bb478f630759768bbd1bfef8ef771a99ab
vksvicky <vksvicky@gmail.com>
2026-08-26 09:33:47 +0100

    chore: Remove unused python scripts, test output, and backup files

--- convert_base.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-img = Image.open('/Users/vivek/.gemini/antigravity-ide/brain/9cac8e9a-aad7-442d-a56b-48032557f0b0/option_a_base.jpg')
-img = img.resize((390, 390), Image.LANCZOS)
-img.save('resources/drawables/dial_bg.png', 'PNG')
